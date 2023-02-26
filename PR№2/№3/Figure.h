62eccf7eec0f89d9e69c2d642c1cfb969a553f38
Sorelim <125604405+Sorelim@users.noreply.github.com>
2023-02-26 22:18:07 +0300

    Add files via upload

---
+++ PR№2/№3/Figure.h
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class Figure {
+private:
+    float radius, x_center, y_center;
+public:
+    Figure(float x1, float x2, float x3, float x4, float y1, float y2, float y3, float y4) {}
+
+    float calculate_side(float x1, float x2, float y1, float y2) {}
+
+    bool is_prug() {}
+
+    void show() {}
+
+    bool is_square() {}
+
+    bool is_romb() {}
+
+    bool is_in_circle() {}
+
+    bool is_out_circle() {}
+};
