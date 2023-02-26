c3cfd2b3c66fbd1dbbd61f55db91cec82f6fcb2f
Sorelim <125604405+Sorelim@users.noreply.github.com>
2023-02-26 21:58:10 +0300

    Add files via upload

---
+++ PR№2/№2/Circle.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class Circle {
+private:
+    float radius, x_center, y_center;
+public:
+    Circle(float r, float x, float y) {}
+
+    void set_circle(float r, float x, float y) {}
+
+    float square() {}
+
+    bool triangle_around(float a, float b, float c) {}
+
+    bool triangle_in(float a, float b, float c) {}
+
+    bool check_circle(float r, float x_cntr, float y_cntr) {}
+
+    void show() {}
+};
