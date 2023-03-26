3831870424309331a1489cf83858e94e84351d2e
Sorelim <125604405+Sorelim@users.noreply.github.com>
2023-03-26 23:09:03 +0300

    Add files via upload

---
+++ PR№6/№2/cone2.cpp
@@ -0,0 +1,46 @@
+#include "cone.h"
+void cone::setCoordinate(double X, double Y, double Z)
+{
+	x = X;
+	y = Y;
+	z = Z;
+}
+void cone::setRadius(double r)
+{
+	R = r;
+}
+void cone::setHeight(double h)
+{
+	H = h;
+}
+void cone::getCoordinate(double& X, double& Y, double& Z)
+{
+	X = x;
+	Y = y;
+	Z = z;
+}
+double cone::getRadius()
+{
+	return R;
+}
+double cone::getHeight()
+{
+	return H;
+}
+double cone::area()
+{
+	return pi * R * (R + sqrt(H * H + R * R));
+}
+double cone::volume()
+{
+	return pi * R * R * H / 3;
+}
+ostream& operator<<(ostream& stream, cone obj)
+{
+	stream << "(" << obj.x << "; ";
+	stream << obj.y << "; ";
+	stream << obj.z << ") ";
+	stream << "r = " << obj.R << " ";
+	stream << "h = " << obj.H << "";
+	return stream;
+}
